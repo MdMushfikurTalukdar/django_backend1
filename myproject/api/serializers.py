2c4be52f77435d0e641713dea5895bbf81377203
Md Mushfikur Talukdar <62092470+MdMushfikurTalukdar@users.noreply.github.com>
2024-11-18 17:41:59 +0600

    Delete myproject directory

--- myproject/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import RoomBooking
-
-class RoomBookingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RoomBooking
-        fields = '__all__'
--- myproject/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import RoomBookingView
-
-urlpatterns = [
-    path('api/roombooking/', RoomBookingView.as_view(), name='room-booking'),
-]
